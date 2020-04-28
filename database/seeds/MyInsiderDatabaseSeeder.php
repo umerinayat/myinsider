ae5f2a12b4e08a83e66580d7a51c7255420b6e98
Umer Inayat <umerinayat.369@gmail.com>
2020-04-28 23:14:33 +0500

    myinsider database seeds changed

--- database/seeds/MyInsiderDatabaseSeeder.php
+++ database/seeds/MyInsiderDatabaseSeeder.php
@@ -11,70 +11,15 @@ class MyInsiderDatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Database Tables Seeds Data
-        for ($i = 1; $i < 50; $i++) 
-        {
-            // Users
-            DB::table('users')->insert([
-                'email' => Str::random(10).'@gmail.com',
-                'password' => bcrypt('secret'),
-                'user_type' => 'client',
-                'is_active' => '1'
-            ]);
-
-            // Contacts
-            DB::table('contacts')->insert([
-                'telephone_number' => '05841 36 46145',
-                'mobile_number' => '+49 69 1234 5678',
-                'country_code' => '+49',
-                'fax_number' => '+44 161 999 8888',
-            ]);
-
-            // Addresses
-            DB::table('addresses')->insert([
-                'Street_address' => 'Lietzenburger Straße 36',
-                'city' => 'Küsten',
-                'state' => 'Niedersachsen',
-                'zip_code' => '29482',
-                'country' => 'Germany',
-            ]);
-
-            // Companies
-            DB::table('companies')->insert([
-                'company_name' => 'Lietzenburger Straße 36',
-                'contact_id' => $i,
-                'address_id' => $i,
-            ]);
-
-            // Clients
-            DB::table('clients')->insert([
-                'title' => 'Mr',
-                'first_name' => Str::random(10),
-                'last_name' => Str::random(10),
-                'user_id' => $i,
-                'company_id' => $i,
-                'contact_id' => $i,
-                'address_id' => $i
-            ]);
-
-            // insiders
-            DB::table('insiders')->insert([
-                'title' => 'Mr',
-                'first_name' => Str::random(10),
-                'last_name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'date_of_birth' => date('Y-m-d'),
-                'national_id_number' => Str::random(10),
-                'language' => 'english',
-                'notes' => Str::random(30),
-                'client_id' => $i,
-                'company_id' => $i,
-                'contact_id' => $i,
-                'address_id' => $i
-            ]);
-        }
-
         
-      
+        // Users
+        DB::table('users')->insert([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('secret'),
+            'user_type' => 'admin',
+            'is_active' => '1'
+        ]);
+
+
     }
 }
